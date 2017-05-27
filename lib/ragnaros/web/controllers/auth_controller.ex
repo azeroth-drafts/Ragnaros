e575977aac1541d08d4e88a5f15bdf2996e97fab
Reductions <slavi.boyanov@gmail.com>
2017-05-27 18:21:24 +0300

    Addin basic auth

--- lib/ragnaros/web/controllers/auth_controller.ex
+++ lib/ragnaros/web/controllers/auth_controller.ex
@@ -1,11 +1,42 @@
 defmodule Ragnaros.Web.AuthController do
   use Ragnaros.Web, :controller
 
-  def auth(conn, %{"user" => user}) do
-    json conn, %{user: user}
+  def auth(conn, %{"user" => user_name}) do
+    #user = Ragnaros.Repo.get_by(Ragnaros.Accounts.User, name: user_name)
+    json conn, (
+      cond do
+        not user_exists?(user) ->
+          %{success: true,
+            token: "token",
+            socket: "socket",
+            message: ""}
+
+        connected?(user) ->
+          %{success: false,
+            token: "",
+            socket: "",
+            message: "Already loged in"}
+
+        true ->
+          %{success: true,
+            token: "",
+            socket: "socket",
+            message: "Already loged in"}
+      end
+    )
   end
 
   def refresh(conn, %{"token" => token}) do
     json conn, %{token: token}
+  end
+
+  def user_exists?(user) do
+    # FIXME
+    true
+  end
+
+  def connected?(user) do
+    # FIXME
+    false
   end
 end
