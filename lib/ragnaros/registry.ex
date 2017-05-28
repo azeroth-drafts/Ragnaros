bb57ff19e0776a90e85fdf51b26265b923e0765f
Nikola Jichev <njichev@gmail.com>
2017-05-28 15:08:00 +0300

    Add drafts, dcek and drafts_finished handling

--- lib/ragnaros/registry.ex
+++ lib/ragnaros/registry.ex
@@ -5,6 +5,15 @@ defmodule Ragnaros.Registry do
     GenServer.call(__MODULE__, {:register, user_id, channel_pid})
   end
 
+  def notify_draft_finished(lobby, game_id) do
+    basic_cards = Ragnaros.Packs.generate_basic()
+    lobby |> Enum.each(fn (user) ->
+      user_cards = Ragnaros.Game.Selection.cards_for_user_and_game(user, game_id)
+      cards = user_cards ++ basic_cards
+      GenServer.cast(__MODULE__, {:notify_draft_finished, user_id, game_id, cards})
+    end)
+  end
+
   def notify_game_canceled(user_id) do
     GenServer.cast(__MODULE__, {:notify_canceled, user_id})
   end
@@ -20,9 +29,8 @@ defmodule Ragnaros.Registry do
   end
 
   def notify_draft(lobby, draft_cards) do
-    Enum.zip(lobby, draft_cards)
-    |>  Enum.each(fn {user_id, cards} ->
-      GenServer.cast(__MODULE__, {:notify_draft_cards, user_id, cards})
+    lobby |> Enum.each(fn user_id ->
+      GenServer.cast(__MODULE__, {:notify_draft_cards, user_id, draft_cards[user_id]})
     end)
   end
 
@@ -61,7 +69,13 @@ defmodule Ragnaros.Registry do
 
   def handle_cast({:notify_draft_cards, user_id, cards}, state) do
     channel_pid = state[user_id]
-    send(channel_pid, :draft_cards)
+    send(channel_pid, {:draft_finish, cards})
+    {:noreply, state}
+  end
+
+  def handle_cast({:notify_draft_finished, user_id, game_id, cards}, state) do
+    channel_pid = state[user_id]
+    send(channel_pid, {:draft_finished, cards})
     {:noreply, state}
   end
 end
