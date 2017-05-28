eaca28130bf93d20aae390ec6d2ed0b91bb4d8db
Reductions <slavi.boyanov@gmail.com>
2017-05-28 05:24:48 +0300

    Fixing channel input

--- lib/ragnaros/web/channels/tavern_channel.ex
+++ lib/ragnaros/web/channels/tavern_channel.ex
@@ -9,17 +9,17 @@ defmodule Ragnaros.Web.TavernChannel do
   `:ignore` to deny subscription/broadcast on this channel
   for the requested topic
   """
-  def join("tavern", %{token: id}, socket) do
+  def join("tavern", %{"token" => id}, socket) do
     Ragnaros.Tavern.join({id, self()})
     {:ok, socket}
   end
 
-  def handle_in("accept", %{token: id}, socket) do
+  def handle_in("accept", %{"token" => id}, socket) do
     Ragnaros.Tavern.accept(id)
     {:noreply, Phoenix.Socket.t}
   end
 
-  def handle_in("reject", %{token: id}, socket) do
+  def handle_in("reject", %{"token" => id}, socket) do
     Ragnaros.Tavern.reject(id)
   end
 
